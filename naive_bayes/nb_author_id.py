ad87841507bb767f712f6ff2f2ed8e9d8455fa3e
Gavin Lewis <epylar@gmail.com>
2016-02-17 19:57:01 -0800

    Checkpoint

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,11 +9,11 @@
     Sara has label 0
     Chris has label 1
 """
-    
-import sys
 from time import time
-sys.path.append("../tools/")
-from email_preprocess import preprocess
+
+from tools.email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -26,6 +26,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time: ", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time: ", round(time()-t0, 3), "s"
+print(accuracy_score(labels_test, pred))
+
+# no. of Chris trining emails: 7936
+# no. of Sara training emails: 7884
+# training time:  1.633 s
+# predicting time:  0.089 s
+# 0.973265073948
 
 
 #########################################################
